9d65025a5e2c564c1a4e76203f5a7633f29189ce
Jacob Magnusson <m@jacobian.se>
2022-07-08 12:02:50 +0200

    fix: Lower default max concurrent downloads

--- src/constants.rs
+++ src/constants.rs
@@ -1,2 +1,2 @@
 pub(crate) const DEFAULT_API_BASE_URL: &str = "https://customerapi.mediaflowpro.com/1/";
-pub(crate) const DEFAULT_MAX_CONCURRENT_DOWNLOADS: u16 = 50;
+pub(crate) const DEFAULT_MAX_CONCURRENT_DOWNLOADS: u16 = 30;
